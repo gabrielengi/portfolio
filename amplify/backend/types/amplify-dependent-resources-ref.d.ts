f1e42a9f608f20a2c607d8f7ac7c514ca9c49225
gabe <gabriel.jsh@gmail.com>
2025-03-17 14:42:26 -0400

    db

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "storage": {
+    "dynamo07b8c820": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports'
+
+// Configure Amplify with aws-exports file
+Amplify.configure(awsconfig)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
